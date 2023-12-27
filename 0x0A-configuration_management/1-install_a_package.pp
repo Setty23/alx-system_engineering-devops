9f41eb55db4cf57369dab1f49466d5796271a90a
Setty23 <125440661+Setty23@users.noreply.github.com>
2023-12-27 10:34:53 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/pup
-# Docs 
-package {'flask':
-  ensure  => '2.1.0',
-  name  =>  'flask',
-  provider => pip3,
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
